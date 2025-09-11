1e0f3c66913777c80e723edf716df599eb90fbbf
Mrinal Satyawanshi <stiffmrinal@gmail.com>
2025-09-12 00:26:41 +0530

    sept 12

---
+++ array/10.majority_element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int prevNum = nums[0], count = 1;
+        int n = nums.length;
+        for(int i=1;i<n;i++){
+            if(nums[i]==prevNum){
+                count++;
+            }
+            else{
+                count--;
+                if(count == 0)
+                 {
+                    prevNum = nums[i];
+                    count++;
+                 }
+            }
+        }
+
+        return prevNum;
+    }
+}
---
+++ array/12.buy_and_sell.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buyPrice = prices[0], sellPrice = prices[0], totalProfit = 0;
+        for(int i =1; i< prices.length; i++){
+            if(prices[i] < buyPrice){
+                totalProfit = Math.max(totalProfit, sellPrice - buyPrice);
+                buyPrice = prices[i];
+                sellPrice = prices[i];
+            }
+            if(prices[i]> sellPrice){
+                sellPrice = prices[i];
+            }
+        }
+
+        totalProfit = Math.max(totalProfit, sellPrice - buyPrice);
+        return totalProfit;
+
+    }
+}
