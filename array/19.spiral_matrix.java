78fd4939c8660aa6a89fd05aeb2911d33f74dd46
Mrinal Satyawanshi <stiffmrinal@gmail.com>
2025-09-20 03:31:14 +0530

    late night code

---
+++ array/19.spiral_matrix.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+        int row = matrix.length, col = matrix[0].length;
+        int top = 0, right = col - 1, down = row - 1, left = 0;
+
+        while (top <= down && left <= right) {
+            // traverse top row
+            for (int i = left; i <= right; i++) {
+                ans.add(matrix[top][i]);
+            }
+            top++;
+
+            // traverse right column
+            for (int i = top; i <= down; i++) {
+                ans.add(matrix[i][right]);
+            }
+            right--;
+
+            // traverse bottom row (check boundary)
+            if (top <= down) {
+                for (int i = right; i >= left; i--) {
+                    ans.add(matrix[down][i]);
+                }
+                down--;
+            }
+
+            // traverse left column (check boundary)
+            if (left <= right) {
+                for (int i = down; i >= top; i--) {
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return ans;
+    }
+}
---
+++ array/20.count_subarray_sum_equals_k.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length, prefixSum = 0, count = 0;
+        Map<Integer, Integer> sum = new HashMap<>();
+        sum.put(0,1);
+        for(int i = 0; i< n; i++){
+            prefixSum += nums[i];
+            int diff = prefixSum - k;
+            if(sum.containsKey(diff)){
+                count += sum.get(diff);
+            }
+            sum.put(prefixSum, sum.getOrDefault(prefixSum, 0) + 1);
+        }
+
+        return count;
+    }
+}
